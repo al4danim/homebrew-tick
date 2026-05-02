803f8019081b0e1621454d102866f9809bcc5140
al4danim <al4danim@qq.com>
2026-05-02 18:33:53 +0800

    Bump tick to 0.5.0

--- Formula/tick.rb
+++ Formula/tick.rb
@@ -1,28 +1,28 @@
 class Tick < Formula
   desc "Lazygit-style TUI for a markdown todo file"
   homepage "https://github.com/al4danim/tick-tui"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.4.0/tick_0.4.0_darwin_x86_64.tar.gz"
-      sha256 "a7c4639a3d3cf4e6b1eeeb009bf7bb5378e91645c32bfd73eb996f911e4900c5"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.0/tick_0.5.0_darwin_x86_64.tar.gz"
+      sha256 "257d350b165647860c4a2e44595b57ff98d405f2564ac4d174d6680f625d043f"
     end
     on_arm do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.4.0/tick_0.4.0_darwin_arm64.tar.gz"
-      sha256 "b753bea89266694320c106f597cff0b1929dddb4309cc7f93f33a691ec1c869c"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.0/tick_0.5.0_darwin_arm64.tar.gz"
+      sha256 "093929be69095c64ee67a76e41e6f9b4563e019d1d0246a84de1ebd07c000d3b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.4.0/tick_0.4.0_linux_x86_64.tar.gz"
-      sha256 "6e24d473b865c6f6e82a652c4d1d41981dfe2f5c0145ecf84475e9f735e34e20"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.0/tick_0.5.0_linux_x86_64.tar.gz"
+      sha256 "17318c36f435243283c40b1c23d205fb5066c39ea17b0f63aacf3004dc197c96"
     end
     on_arm do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.4.0/tick_0.4.0_linux_arm64.tar.gz"
-      sha256 "1d1124763617d49956443c20d47e18be6f4c2833f06c7b58f205e08a62de4638"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.0/tick_0.5.0_linux_arm64.tar.gz"
+      sha256 "926cd61a106ba8fba89ae95aadcd821d5e73dce1df4d109ad061346095a39830"
     end
   end
 
