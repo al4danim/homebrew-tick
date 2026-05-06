398c487d65848e7d4ce4dbf310ce38a4bdbcd6ec
al4danim <al4danim@gmail.com>
2026-05-07 00:22:56 +0800

    tick 0.6.0 (fix: update Formula/tick.rb, remove stale root tick.rb)

--- Formula/tick.rb
+++ Formula/tick.rb
@@ -1,28 +1,28 @@
 class Tick < Formula
   desc "Lazygit-style TUI for a markdown todo file"
   homepage "https://github.com/al4danim/tick-tui"
-  version "0.5.2"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.2/tick_0.5.2_darwin_x86_64.tar.gz"
-      sha256 "5f37ba0869683bef3ca4111c052d1fc01cb7f69de619857f87ecaa5c167fd1ab"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.6.0/tick_0.6.0_darwin_x86_64.tar.gz"
+      sha256 "3a00983ceac7dc41578c87c53ff5a7bbc8e2aba2da843e13aa96b8af2aed69fc"
     end
     on_arm do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.2/tick_0.5.2_darwin_arm64.tar.gz"
-      sha256 "84386113f1a521a4b07c9c08995adf82496da18f64da7319b1ab6183857e7b3d"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.6.0/tick_0.6.0_darwin_arm64.tar.gz"
+      sha256 "f4fd25b56a1eef25bad588e588229e625cc69db19a65d385ddad9e38aa73b788"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.2/tick_0.5.2_linux_x86_64.tar.gz"
-      sha256 "526a87ffdcfa450cce61035e9622ce2889576d7e65cd25563cbdb0a223c730f2"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.6.0/tick_0.6.0_linux_x86_64.tar.gz"
+      sha256 "91a20ab490f9449c94975efe2e7085b541a4bc1886a664a3643071e634d4d2e3"
     end
     on_arm do
-      url "https://github.com/al4danim/tick-tui/releases/download/v0.5.2/tick_0.5.2_linux_arm64.tar.gz"
-      sha256 "0cdf0f39a8ce664e66aea2a9db9d8e51ac56b860d1faddbc43a87633e15b095e"
+      url "https://github.com/al4danim/tick-tui/releases/download/v0.6.0/tick_0.6.0_linux_arm64.tar.gz"
+      sha256 "d8ca40fed91fb4869c108e3ee3d61e8a4987d109feba3e90d599d9005faa4038"
     end
   end
 
